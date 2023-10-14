54913980f3bc266db5db1e4c86d8d259f49e0d2b
Gabriel Albino <82725552+gabrielalb5@users.noreply.github.com>
2023-10-14 19:59:47 -0300

    Add files via upload

---
+++ Get Frete/src/notif_apagar.php
@@ -0,0 +1,5 @@
+<?php
+    require("../database/funcoes.php");
+    notif_apagar_tudo();
+    header("Location:../public/index.php");
+?>
---
+++ Get Frete/src/notif_lidas.php
@@ -0,0 +1,5 @@
+<?php
+    require("../database/funcoes.php");
+    notif_marcar_tudo_lido();
+    header("Location:../public/index.php");
+?>
